b083e1fd1ee291ef1b9b04c109e3eb57342c6455
arju88nair <arju88nair@gmail.com>
2019-08-24 22:32:16 +0530

    json and jsonify

--- cah.py
+++ cah.py
@@ -1,9 +1,14 @@
-from flask import Flask
+import os
+from flask import Flask,render_template, url_for, json,jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def greeting():
-    return "<h1 style='color:green'>Hello Kriti!</h1>"
+def index():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "", "cards.json")
+    data = json.load(open(json_url))
+    return jsonify(data)
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
