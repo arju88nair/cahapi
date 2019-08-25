85a5d450568270dcfc4caef0abdb60837b59e7c5
arju88nair <arju88nair@gmail.com>
2019-08-25 22:17:14 +0530

    docstring

--- cah.py
+++ cah.py
@@ -4,15 +4,34 @@ from flask import Flask,render_template, url_for, json,jsonify
 app = Flask(__name__)
 
 
+"""[Default route for the main endpoint returning json ]
+
+Returns:
+    [json] 
+"""
 @app.route("/")
 def index():
+    
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "", "cards.json")
     data = json.load(open(json_url))
     payload=payloadBuilder(data)
     return jsonify(payload)
+
+
+
+
     
 def payloadBuilder(data):
+"""[Method to buold the payload]
+    
+    Arguments:
+        data {[json]}
+    
+    Returns:
+        [payload] -- [json object]
+    """
+    
     decks=data['order']
     blackCards=data['blackCards']
     whiteCards=data['whiteCards']
