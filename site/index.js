2eb36fb22281451b4fc98b39debf29ee11fc8da9
arju88nair <arju88nair@gmail.com>
2019-08-28 23:11:19 +0530

    animation black cardx

--- site/index.js
+++ site/index.js
@@ -1,12 +1,15 @@
 function doCall() {
-    $(".dark").fadeOut();
-    $(".dark").fadeIn();
+
 
     $.ajax({
         url: "http://127.0.0.1:5000/",
         type: 'GET',
         dataType: 'json', //you may use jsonp for cross origin request
         success: function(res) {
+            $(".dark").fadeOut();
+            $(".dark-text").text(res[0]['blackCard'])
+            $(".dark").fadeIn();
+
             console.log(res[0]['blackCard']);
         },
         error: function(err) {
