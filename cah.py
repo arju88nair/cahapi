6ef00c2e3a4b61dea7a7fe0380522dfbc43291c5
arju88nair <arju88nair@gmail.com>
2019-08-28 16:36:42 +0530

    banner

--- cah.py
+++ cah.py
@@ -23,6 +23,7 @@ def index():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "", "cards.json")
     data = json.load(open(json_url))
+    
     payload=payloadBuilder(data)
     return jsonify(payload)
 
