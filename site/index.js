24245dc8c648eb220ba94cc7d26d95aef38eec9e
arju88nair <arju88nair@gmail.com>
2019-08-31 23:17:44 +0530

    card animation

--- site/index.js
+++ site/index.js
@@ -1,19 +1,21 @@
 function doCall() {
 
 
-    // $(".dark").fadeOut();
-    $.ajax({
-        url: "http://127.0.0.1:5000/",
-        type: 'GET',
-        dataType: 'json', //you may use jsonp for cross origin request
-        success: function(res) {
-            $(".dark").text(Math.random())
-            $(".dark").fadeIn();
+    $(".dark").fadeOut(500, function() {
+        $.ajax({
+            url: "http://127.0.0.1:5000/",
+            type: 'GET',
+            dataType: 'json', //you may use jsonp for cross origin request
+            success: function(res) {
 
-            console.log(res[0]['blackCard']);
-        },
-        error: function(err) {
-            console.log(err)
-        }
+                $(".dark-text").text(res[0]['blackCard'])
+            },
+            error: function(err) {
+                console.log(err)
+            }
+        });
+        $(".dark").fadeIn(500);
     });
+
+
 };
